8ff4ccbea2775715c63b351a90b85a7002cdc84a
tristan <perry17522@gapps.uwcsea.edu.sg>
2023-08-30 18:04:12 +0800

    stinky

---
+++ app/src/main/java/com/example/killmealready/mainPage.kt
@@ -0,0 +1,38 @@
+package com.example.killmealready
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.killmealready.ui.theme.KillMeAlreadyTheme
+@Preview
+@Composable
+fun mainPage(){
+    Box(modifier = Modifier.fillMaxSize()){
+        Column{
+            Row{
+                Text(text = "Progress")
+                Text(text = "Home")
+                Text(text = "Archive")
+
+            }
+        }
+
+
+    }
+    fun Home(){
+
+    }
+}
