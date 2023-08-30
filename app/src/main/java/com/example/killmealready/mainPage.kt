5439cc45eea8e22eec25a1c2d71e788106ecedcb
tristan <perry17522@gapps.uwcsea.edu.sg>
2023-08-30 19:08:52 +0800

    2

--- app/src/main/java/com/example/killmealready/mainPage.kt
+++ app/src/main/java/com/example/killmealready/mainPage.kt
@@ -13,19 +13,64 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.*
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+
+
+
 
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.killmealready.ui.theme.KillMeAlreadyTheme
 @Preview
 @Composable
 fun mainPage(){
-    Box(modifier = Modifier.fillMaxSize()){
-        Column{
-            Row{
-                Text(text = "Progress")
-                Text(text = "Home")
-                Text(text = "Archive")
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.TopCenter
+    ){
+        Column(
+            modifier = Modifier.padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(16.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                //make each text a button to move pages
+                Text(
+                    text = "Progress",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp,
+                    modifier = Modifier.padding(8.dp)
+                )
+                Text(
+                    text = "Home",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp,
+                    modifier = Modifier.padding(8.dp)
+                )
+                Text(
+                    text = "Archive",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp,
+                    modifier = Modifier.padding(8.dp)
+                )
 
+            Text(
+            text = "Add Meal",
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp,
+            modifier = Modifier.padding(bottom = 8.dp)
+            )
             }
         }
 
